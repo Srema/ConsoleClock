cc9d8e42459dd1381f54c375c56d087c6c9f55b5
sonol02 <sonol02@DESKTOP-914S3J4>
2018-11-03 12:52:03 +0900

    Util함수 추가 - 학원

---
+++ ConsoleClock/Util.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+
+#include <Windows.h>			//Console관련 함수를 활용하기 위하여
+#include <stdio.h>
+#include "Util.h"
+
+void CursorUtil_GotoXY(int x, int y)
+{
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	
+}
+
+void CursorUtil_Hide()
+{
+
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.dwSize = 1;
+	cursorInfo.bVisible = FALSE;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
---
+++ ConsoleClock/util.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define WINDOW_LINE_SIZE 9
+#define WINDOW_COL_SIZE 66
+
+//커서위치를 옮기는 함수
+void CursorUtil_GotoXY(int x, int y);
+
+
+//커서를 감추는 함수
+void CursorUtil_Hide();
+
+
+//ConsolWindow의 크기를 조정하는 함수
+void IndowUtil_ChangeWindowSize(int lines, int cols);
+
+
+//콘솔 폰트 색을 바꾸는 함수
+void FontUtil_ChangeFontColor(int color);
