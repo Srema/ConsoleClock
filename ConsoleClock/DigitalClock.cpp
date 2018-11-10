4e72e7da69f066e233edd24f5a01475bea5be94c
sonol02 <sonol02@DESKTOP-914S3J4>
2018-11-10 12:56:05 +0900

    DigitalClock struct 정의 DigitalClock 가져오는 함수 수정 약간의 필요한 define 추가

--- ConsoleClock/DigitalClock.cpp
+++ ConsoleClock/DigitalClock.cpp
@@ -5,11 +5,22 @@
 #include <time.h>
 
 
-void DigitalClock_GetTime()
+DigitalClock DigitalClock_GetTime()
 {
 	time_t now = time(NULL);
 	struct tm time;
 	localtime_s(&time, &now);
 
 	printf("%04d-%02d-%02d %02d:%02d:%02d", time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, time.tm_hour, time.tm_min, time.tm_sec);
+
+	DigitalClock dgc = { 0, };
+	dgc.year = time.tm_year + 1900;
+	dgc.month = time.tm_mon + 1;
+	dgc.day0fMonth = time.tm_mday;
+	dgc.day0fWeek = time.tm_wday;
+	dgc.hour = time.tm_hour;
+	dgc.minute = time.tm_min;
+	dgc.second = time.tm_sec;
+
+	return dgc;
 }
--- ConsoleClock/DigitalClock.h
+++ ConsoleClock/DigitalClock.h
@@ -1,3 +1,40 @@
 #pragma once
 
-void DigitalClock_GetTime();
+enum {
+	HOUR_LEFT,	//1
+	HOUR_RIGHT,	//2
+	
+	DELIMITER_BETWEEN_HOUR_AND_MINUTE,	//3
+	MINUTE_LEFT,	//4
+	MINUTE_RIGHT,	//5
+	
+	DELIMITER_BETWEEN_MINUTE_AND_SECOND,	//6
+	SECOND_LEFT,	//7
+	SECOND_RIGHT	//8
+};
+
+//      ½Ã°£  02:05:32
+//            12345678 
+
+typedef struct _digitalClock
+{
+	int year;
+	int month;
+	int day0fMonth;
+	int day0fWeek;   //sunday(0)    monday(1)   ...
+	int hour;
+	int minute;
+	int second;
+}DigitalClock;
+
+#define ONE_SECOND 1000
+#define INITIAL_X_POSITION_FOR_TIME 2
+#define INITIAL_Y_POSITION_FOR_TIME 3
+#define Y_OFFSET_FOR_TIME 8
+#define INITIAL_X_POSITION_FOR_DATE 22
+#define INITIAL_Y_POSITION_FOR_DATE 1
+#define TRUE 1
+#define FALSE 0
+
+DigitalClock DigitalClock_GetTime();
+void DigitalClock_print(DigitalClock DigitalClock);
